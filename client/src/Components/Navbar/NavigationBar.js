20ea0298ae0efa14e0c119f3a6f6943f297d551d
rwycress <rwycress@gmail.com>
2021-05-22 23:14:01 +0900

    audio edit 페이지 bootstrap card layout 적용

--- client/src/Components/Navbar/NavigationBar.js
+++ client/src/Components/Navbar/NavigationBar.js
@@ -54,10 +54,10 @@ const NavBar = ({ hasCookie, setHasCookie, removeCookie }) => {
           <></>
         )}
 
-        <Form inline>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
-        </Form>
+        </Form> */}
       </Navbar.Collapse>
     </Navbar>
   );
--- client/src/Pages/AudioEdit.js
+++ client/src/Pages/AudioEdit.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Button, Container, Table, Modal } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Container,
+  Table,
+  Modal,
+  Form,
+  FormGroup,
+  FormControl,
+} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactAudioPlayer from "react-audio-player";
 import AudioUpload from "../Components/AudioUpload/AudioUpload";
+import playingImg from "../Assets/Images/musicplaying.jpg";
 
 const AudioEdit = ({ setHasCookie, removeCookie }) => {
   const [audioList, setAudioList] = useState([]);
@@ -30,19 +40,23 @@ const AudioEdit = ({ setHasCookie, removeCookie }) => {
     }).then((response) => response.json());
   };
 
-  const handleGetAudioList = async () => {
-    let userId = localStorage.getItem("userId");
-    const response = await getAudioListApi(userId);
-    console.log("handleGetAudioList >>>>>>>>>>");
-    console.log(response);
+  const handleGetAudioList = () => {
+    let userId = "";
+    setTimeout(async () => {
+      userId = localStorage.getItem("userId");
 
-    if (response.result === "token expired") {
-      setHasCookie(false);
-    } else {
-      setAudioList(response.data);
-    }
+      const response = await getAudioListApi(userId);
+      console.log("handleGetAudioList >>>>>>>>>>");
+      console.log(response);
 
-    setNewAudioFlag(false);
+      if (response.result === "token expired") {
+        setHasCookie(false);
+      } else {
+        setAudioList(response.data);
+      }
+
+      setNewAudioFlag(false);
+    }, 50);
   };
 
   const playAudioApi = (trackName, userId) => {
@@ -145,7 +159,32 @@ const AudioEdit = ({ setHasCookie, removeCookie }) => {
 
       <Container>
         <h3>음악 리스트</h3>
-        <Container>
+
+        {/* <Container>
+          <ReactAudioPlayer src={selectedAudio} autoPlay controls />
+        </Container> */}
+
+        <Card className="my-5">
+          <Card.Header>Now Playing</Card.Header>
+          <Card.Body>
+            <Card.Img src={playingImg} className="w-75 d-flex mx-auto my-3" />
+            <ReactAudioPlayer
+              src={selectedAudio}
+              autoPlay
+              controls
+              style={{ width: "100%" }}
+            />
+          </Card.Body>
+        </Card>
+
+        {/* <Container className="w-100 my-3 d-flex flex-row-reverse">
+          <Button
+            variant="primary"
+            onClick={handleAudioSearch}
+            style={{ marginLeft: "5px" }}
+          >
+            SEARCH
+          </Button>
           <input
             type="text"
             name="search"
@@ -154,109 +193,127 @@ const AudioEdit = ({ setHasCookie, removeCookie }) => {
             }}
             placeholder="곡 제목"
           />
-          <Button
-            variant="primary"
-            onClick={handleAudioSearch}
-            style={{ marginLeft: "5px" }}
-          >
-            SEARCH
-          </Button>
-        </Container>
-        <Container>
-          <ReactAudioPlayer src={selectedAudio} autoPlay controls />
-        </Container>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>NO</th>
-              <th>TRACK NAME</th>
-              <th>TITLE</th>
-              <th>ALBUM</th>
-              <th>ARTIST</th>
-              <th>ACTION</th>
-            </tr>
-          </thead>
-          <tbody>
-            {!goSearch &&
-              audioList.map((v, i) => {
-                return (
-                  <tr key={v.SEQ}>
-                    <td>{i + 1}</td>
-                    <td
-                      style={{
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                        maxWidth: "500px",
-                        overflow: "hidden",
-                      }}
-                    >
-                      {v.TRACK_NAME}
-                    </td>
-                    <td>{v.TITLE}</td>
-                    <td>{v.ALBUM}</td>
-                    <td>{v.ARTIST}</td>
-                    <td>
-                      <Button
-                        variant="primary"
-                        onClick={handlePlayAudio}
-                        style={{ marginRight: "5px" }}
-                      >
-                        PLAY
-                      </Button>
-                      <Button
-                        variant="primary"
-                        onClick={handleEditAudio}
-                        style={{ marginLeft: "5px" }}
-                      >
-                        EDIT
-                      </Button>
-                    </td>
-                  </tr>
-                );
-              })}
-            {goSearch &&
-              audioList
-                .filter((item) => {
-                  return item.TRACK_NAME.includes(searchKeyword);
-                })
-                .map((v, i) => {
-                  return (
-                    <tr key={v.SEQ}>
-                      <td>{i + 1}</td>
-                      <td
-                        style={{
-                          textOverflow: "ellipsis",
-                          whiteSpace: "nowrap",
-                          maxWidth: "500px",
-                          overflow: "hidden",
-                        }}
-                      >
-                        {v.TRACK_NAME}
-                      </td>
-                      <td>{v.TITLE}</td>
-                      <td>{v.ALBUM}</td>
-                      <td>{v.ARTIST}</td>
-                      <td>
-                        <Button
-                          variant="primary"
-                          onClick={handlePlayAudio}
-                          style={{ marginRight: "5px" }}
+        </Container> */}
+
+        <Card className="my-5">
+          <Card.Header>Playlist</Card.Header>
+          <Card.Body>
+            <Form>
+              <FormGroup
+                controlId="search"
+                className="w-50 my-3 d-flex float-right"
+              >
+                <FormControl
+                  type="text"
+                  placeholder="곡 제목 검색"
+                  onChange={(e) => {
+                    setSearchKeyword(e.target.value);
+                  }}
+                />
+                <Button
+                  variant="primary"
+                  onClick={handleAudioSearch}
+                  style={{ marginLeft: "5px" }}
+                >
+                  SEARCH
+                </Button>
+              </FormGroup>
+            </Form>
+
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>NO</th>
+                  <th>TRACK NAME</th>
+                  <th>TITLE</th>
+                  <th>ALBUM</th>
+                  <th>ARTIST</th>
+                  <th>ACTION</th>
+                </tr>
+              </thead>
+              <tbody>
+                {!goSearch &&
+                  audioList.map((v, i) => {
+                    return (
+                      <tr key={v.SEQ}>
+                        <td>{i + 1}</td>
+                        <td
+                          style={{
+                            textOverflow: "ellipsis",
+                            whiteSpace: "nowrap",
+                            maxWidth: "500px",
+                            overflow: "hidden",
+                          }}
                         >
-                          PLAY
-                        </Button>
-                        <Button
-                          variant="primary"
-                          onClick={handleEditAudio}
-                          style={{ marginLeft: "5px" }}
-                        >
-                          EDIT
-                        </Button>
-                      </td>
-                    </tr>
-                  );
-                })}
-          </tbody>
-        </Table>
+                          {v.TRACK_NAME}
+                        </td>
+                        <td>{v.TITLE}</td>
+                        <td>{v.ALBUM}</td>
+                        <td>{v.ARTIST}</td>
+                        <td>
+                          <Button
+                            variant="primary"
+                            onClick={handlePlayAudio}
+                            style={{ marginRight: "5px" }}
+                          >
+                            PLAY
+                          </Button>
+                          <Button
+                            variant="primary"
+                            onClick={handleEditAudio}
+                            style={{ marginLeft: "5px" }}
+                          >
+                            EDIT
+                          </Button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                {goSearch &&
+                  audioList
+                    .filter((item) => {
+                      return item.TRACK_NAME.includes(searchKeyword);
+                    })
+                    .map((v, i) => {
+                      return (
+                        <tr key={v.SEQ}>
+                          <td>{i + 1}</td>
+                          <td
+                            style={{
+                              textOverflow: "ellipsis",
+                              whiteSpace: "nowrap",
+                              maxWidth: "500px",
+                              overflow: "hidden",
+                            }}
+                          >
+                            {v.TRACK_NAME}
+                          </td>
+                          <td>{v.TITLE}</td>
+                          <td>{v.ALBUM}</td>
+                          <td>{v.ARTIST}</td>
+                          <td>
+                            <Button
+                              variant="primary"
+                              onClick={handlePlayAudio}
+                              style={{ marginRight: "5px" }}
+                            >
+                              PLAY
+                            </Button>
+                            <Button
+                              variant="primary"
+                              onClick={handleEditAudio}
+                              style={{ marginLeft: "5px" }}
+                            >
+                              EDIT
+                            </Button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
 
         <Modal size="xl" show={modalShow} onHide={handleModalClose}>
           <Modal.Header closeButton>
